87b8ca98da0fa7140f35a02ba159f231ff9ecd2e
JPTrey <jptrey91@gmail.com>
2024-02-13 23:54:22 -0500

    Move filtering behavior to separate file.

--- src/components/GameList.tsx
+++ src/components/GameList.tsx
@@ -1,50 +1,30 @@
-import { recByFriend } from "../functions";
+import { getFilteredGames } from "../functions";
 import { useGetGamesQuery } from "../store/gamesApi";
 import { useAppSelector } from "../store/hooks";
-import { FriendsOfFenAster } from "../store/preferenceSlice";
 import ErrorDisplay from "./ErrorDisplay";
 import GameListItem from "./GameListItem";
 
 const GameList = () => {
   const { data, isLoading, error } = useGetGamesQuery();
-  const {
-    showCompleted,
-    showRecsTo,
-    showRecsBy,
-    showOnlyFriends,
-    columnCount,
-  } = useAppSelector((state) => state.preference);
+  const { columnCount } = useAppSelector((state) => state.preference);
 
   let content;
 
   if (isLoading) content = <div>Loading...</div>;
   else if (error) content = <ErrorDisplay error={error} />;
   else {
-    // filter games based on selected preferences
-    let filteredGames = [...data!];
-    if (!showCompleted)
-      filteredGames = filteredGames!.filter((game) => !game.wasCompleted);
-    if (showRecsTo !== "both")
-      filteredGames = filteredGames!.filter((game) =>
-        showRecsTo.includes(game.recTo)
+    const filteredGames = getFilteredGames(data!);
+
+    if (filteredGames.length < 1)
+      content = <div className="w-3/4">No games match this criteria.</div>;
+    else
+      content = (
+        <div className={`grid grid-cols-${columnCount} gap-3`}>
+          {filteredGames?.map((game) => (
+            <GameListItem key={game.id} game={game} />
+          ))}
+        </div>
       );
-
-    if (showRecsBy.length > 0) {
-      filteredGames = filteredGames!.filter((game) =>
-        showRecsBy.includes(game.recBy)
-      );
-    }
-
-    if (showOnlyFriends)
-      filteredGames = filteredGames!.filter((game) => recByFriend(game));
-
-    content = (
-      <div className={`grid grid-cols-${columnCount} gap-3`}>
-        {filteredGames?.map((game) => (
-          <GameListItem key={game.id} game={game} />
-        ))}
-      </div>
-    );
   }
 
   return content;
--- src/functions.ts
+++ src/functions.ts
@@ -1,5 +1,36 @@
 import { Game } from "./entities/Game";
 import { FriendsOfFenAster } from "./store/preferenceSlice";
+import { store } from "./store/store";
 
 export const recByFriend = (game: Game): boolean =>
   FriendsOfFenAster.includes(game.recBy);
+
+export const getFilteredGames = (games: Game[]): Game[] => {
+  const { showCompleted, showRecsTo, showRecsBy, showOnlyFriends, searchTerm } =
+    store.getState().preference;
+
+  let filteredGames = [...games];
+
+  if (!showCompleted)
+    filteredGames = filteredGames.filter((game) => !game.wasCompleted);
+
+  if (showRecsTo !== "both")
+    filteredGames = filteredGames.filter((game) =>
+      showRecsTo.includes(game.recTo)
+    );
+
+  if (showRecsBy.length > 0)
+    filteredGames = filteredGames.filter((game) =>
+      showRecsBy.includes(game.recBy)
+    );
+
+  if (showOnlyFriends)
+    filteredGames = filteredGames.filter((game) => recByFriend(game));
+
+  if (searchTerm.length > 3)
+    filteredGames = filteredGames.filter((game) =>
+      game.title.includes(searchTerm)
+    );
+
+  return filteredGames;
+};
